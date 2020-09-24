3a586ee368264615fb878be9bc6f0a93e9bbad51
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-09-24 11:00:58 -0400

    Added base outline of PIC controller

---
+++ include/controller.h
@@ -0,0 +1,21 @@
+/* @file controller.h
+ *
+ */
+
+class PIDController
+{
+  public:
+    PIDController();
+    ~PIDController();
+
+    /* @brief Calculate command given setpoint and error.
+     */
+    double compute(double setpoint, double velocity);
+
+  private:
+    // control parameters
+    double kp_, ki_, kd_;
+
+}
+
+    
