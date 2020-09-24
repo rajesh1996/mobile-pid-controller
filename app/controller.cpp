c76410124f4278d3bca377f03cd4c4dc61c548fa
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-09-24 12:57:48 -0400

    Stub implementation

--- app/controller.cpp
+++ app/controller.cpp
@@ -5,3 +5,28 @@ double PIDController::compute(double setpoint, double velocity)
 {
     return 0;
 }
+
+// stub implementation
+void PIDController::reset()
+{
+}
+
+// stub implementation
+void PIDController::setK(double kp, double ki, double kd)
+{
+}
+
+// stub implementation
+void PIDController::setT(double dt)
+{
+}
+
+// stub implementation
+void PIDController::getK(double& kp, double& ki, double& kd)
+{
+}
+
+// stub implementation
+void PIDController::getT(double& dt)
+{
+}
