0066a076cbc84de033a63dd9d52d0cb2056beb5a
rajesh <rns@umd.edu>
2020-09-27 15:45:04 -0400

    Updating main wih sample user test case

--- app/main.cpp
+++ app/main.cpp
@@ -2,9 +2,28 @@
  * @copyright [2020] <Daniel M. Sahu> <Spencer Elyard> 
  */
 
+#include <iostream>
 #include <controller.h>
 
 int main() {
+  PIDController controller;
+  double desired, actual;
+  double kp = 0.5;
+  double ki = 0.1;
+  double kd = 0.2;
+  double dt = 0.01;
+  std::cout << "Enter the desired velocity: ";
+  std::cin >> desired;
 
+  std::cout << "ENter the actual velocity: ";
+  std::cin >> actual;
+
+  //call the setter function
+  controller.setK(kp, ki, kd);
+  controller.setT(dt);
+
+  //call the compute method
+  double new_output = controller.compute(desired, actual);
+  std::cout << "output of the PID controller= " << new_output << std::endl;
   return 0;
 }
